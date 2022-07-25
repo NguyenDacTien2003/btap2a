e5d1b5271a13c76a0706ec83662375649cae72cc
NguyenDacTien2003 <103101867+NguyenDacTien2003@users.noreply.github.com>
2022-07-25 03:10:27 -0700

    Update Ex3.cs

--- ex3/Ex3.cs
+++ ex3/Ex3.cs
@@ -1,2 +1,27 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+class MainClass
+{
+    public static int AnExceptionFunction (int value)
+    {
+        if (value == 0)
+            throw new DivideByZeroException("Divide By 0 error ");
+        int x = 20;
+        return x;
+    }
+    public static void Main ()
+    {
+        int value = 0;
+        try
+        {
+            value = AnExceptionFunction(10);
+            Console.WriteLine("Value = {0}",value);
+            AnExceptionFunction(0);
+            Console.WriteLine("Value = {0}", value);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Caught an exeption {0}.Continuing", e);
+        }
+        Console.WriteLine("Done");
+    }
+}
