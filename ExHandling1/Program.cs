258fa755710a299699ae218fa2ee1ab619aae7eb
NguyenDacTien2003 <103101867+NguyenDacTien2003@users.noreply.github.com>
2022-07-25 03:06:09 -0700

    baitap

---
+++ ExHandling1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+namespace Bai05
+{
+    class vidu1
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[5];
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("a[{0}] = ", i + 1);
+                    a[i] = Convert.ToByte(Console.ReadLine());
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Khong duoc nhap ki tu so");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Khong duoc nhap gia trij nam ngoai 0-225");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("loi vuot qua vi pham");
+            }
+            for(int i = 0; i < 5; i++)
+                Console.WriteLine("{0}", a[i]);
+        }
+
+            }
+        }
+   
